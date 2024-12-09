91993f646bc3c2e91a503df114cc1169867372b8
vuth9808 <vuth9808@ut.edu.vn>
2024-12-09 22:04:17 +0700

    update4

---
+++ products.js
@@ -0,0 +1,14 @@
+class Products {
+    constructor(name, price, screen, blackCamera, frontCamera, img, desc, type) {
+        this.name = name;
+        this.price = price;
+        this.screen = screen;
+        this.blackCamera = blackCamera;
+        this.frontCamera = frontCamera;
+        this.img = img;
+        this.desc = desc;
+        this.type = type;
+    }
+}
+
+export default Products;
